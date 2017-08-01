b8f82bd18c9b10715b94e914da2885072a8631ac
namsanghyoun <skatkdgus7@daum.net>
2017-08-01 14:56:16 +0000

    n

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714102109) do
+ActiveRecord::Schema.define(version: 20170730123514) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
